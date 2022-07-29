bdcc311080fd8aadfe16c9d90c60f5f3661cc419
Moiamiok <qlf.mnaiduc,2017@gmail.com>
2022-07-30 03:54:55 +0700

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+//	bai1
+//	int number;
+//	cin >> number;
+//	int m[50];
+//	int sum;
+//	for(int i=0;i<number;i++){
+//		cout<<"nhap so "<<i+1<<" : ";
+//		cin>> m[i];
+//		sum+=m[i];
+//	}
+//	cout<<sum;
+//	for(int i=0;i<number;i++){
+//		cout<<","<<m[i];
+//	}
+//		bai2
+//	int sont;
+//	cout<<"nhap so co 4 chu so: ";
+//	cin>> sont;
+//	int sum=0;
+//	int start,end;
+//	for(int i=0;i<4;i++){
+//		if(i==0){
+//			end=sont%10;
+//		}
+//		if(i==3){
+//			start=sont%10;
+//		}
+//		sum=sum+sont%10;
+//		sont=sont/10;
+//	}
+//	cout<<sum<<","<<start<<","<<end;
+//	bai 3
+//	int tien;
+//	int a,b=0,c,d;
+//	cin>>tien;
+//	while(tien>=10){
+//		if(tien>=100){
+//			tien=tien-100;
+//			a+=1;
+//		}else if(tien>=50&&tien<100){
+//			tien=tien-50;
+//			b=b+1;
+//		}else if(tien>=20&&tien<50){
+//			tien=tien-20;
+//			c+=1;
+//		}else if(tien>=10&&tien<20){
+//			tien=tien-10;
+//			d+=1;
+//		}      
+//	}
+//	cout<<a<<" 100"<<endl<<b<<" 50"<<endl<<c<<" 20"<<endl<<d<<" 10";
+//	bai 4
+//	int a,b,c;
+//	cin>>a>>b>>c;
+//	if(a+b>c&&a+c>b&&b+c>a){
+//		cout<<"la mot tam giac ";
+//		if(a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a){
+//			cout<<"vuong";
+//		}else if(a==b&&b==c){
+//			cout<<"deu";
+//		}else if(a==b||b==c||c==a){
+//			cout<<"can";
+//		}else{
+//		cout<<"thuong";
+//		}
+//	}else {
+//		cout<<"ko phai la tam giac";
+//	}
+//	bai 5
+//	int day;
+//	cin>>day;
+//	if(day<8&&day>=2){
+//		cout<<"thu "<<day;
+//	}else if(day==8){
+//		cout<<"chu nhat";
+//	}else{
+//	cout<<"?";
+//	}
+	
+	
+	return 0;
+}
